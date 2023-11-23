43f4c5150b16a36963ce48259699b1035fdc9362
nligade78 <nikhilligade22@gmail.com>
2023-11-23 09:20:47 +0530

    update SpringMVC JPA project

---
+++ springmvcboot/src/main/java/com/web/springmvcboot/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.web.springmvcboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class MainController {
+
+    @RequestMapping("/")
+    public String home()
+    {
+        System.out.println("This is Home Page");
+        return " Home Controller ";
+    }
+}
