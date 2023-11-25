d9cac212397b2be76ed2908464b95bf4be46b933
nligade78 <nikhilligade22@gmail.com>
2023-11-25 23:12:10 +0530

    Using JPQL retrieve data from Database

--- SpringJPA/src/main/java/com/JPA/SpringJPA/repository/UserRepository.java
+++ SpringJPA/src/main/java/com/JPA/SpringJPA/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.JPA.SpringJPA.repository;
 
 import com.JPA.SpringJPA.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,6 +11,14 @@ public interface UserRepository extends CrudRepository<User,Integer> {
 
     public List<User> findByName(String name);
     public List<User> findByCity(String city);
+
+    //Jpql query
+    @Query("select u FROM User u")
+    public List<User> getAllUser();
+
+    @Query("select u From User u WHERE u.name=:n and u.city=:c")
+    public List<User> getUserByName(@Param("n") String name,@Param("c") String city); //n Parameter bounded
+
 
 
 }
