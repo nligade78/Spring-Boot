56b8913a3f65fcdbe68cf70adfa9f9f662579b28
nligade78 <nikhilligade22@gmail.com>
2023-11-26 20:27:57 +0530

    Understanding Get-mapping and RestController

---
+++ RestBookAPI/src/main/java/com/restBook/RestBookAPI/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.restBook.RestBookAPI.controller;
+
+import com.restBook.RestBookAPI.entities.Book;
+import com.restBook.RestBookAPI.services.BookServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private BookServices bookServices;
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public String getBooks()
+    {
+        return "This is testing book";
+    }
+
+    @GetMapping("/data")
+    public List<Book> getAllBook()
+    {
+        return this.bookServices.getBookList();
+    }
+
+    @GetMapping("/book/{id}")
+    public Book getBookById(@PathVariable("id") int id)
+    {
+        return this.bookServices.getBookById(id);
+    }
+}
---
+++ RestBookAPI/src/main/java/com/restBook/RestBookAPI/services/BookServices.java
@@ -0,0 +1,31 @@
+package com.restBook.RestBookAPI.services;
+
+import com.restBook.RestBookAPI.entities.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookServices {
+
+    private static List<Book> list = new ArrayList<Book>();
+
+    static {
+        list.add(new Book(12,"Javascript","XYZ"));
+        list.add(new Book(32,"HTML","PQR"));
+        list.add(new Book(62,"CSS","ABC"));
+    }
+
+    //get all book
+    public List<Book> getBookList() {
+        return list;
+    }
+
+    //get single book by id
+    public Book getBookById(int id) {
+        Book book=null;
+         book=list.stream().filter(e->e.getId()==id).findFirst().get();
+         return book;
+    }
+}
