ef5175f5428cba747d81b553059f4b6471d325bc
nligade78 <nikhilligade22@gmail.com>
2023-11-26 17:26:01 +0530

    Project Started Spring Rest API

---
+++ RestBookAPI/src/main/java/com/restBook/RestBookAPI/RestBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.restBook.RestBookAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBookApiApplication.class, args);
+	}
+
+}
---
+++ RestBookAPI/src/test/java/com/restBook/RestBookAPI/RestBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restBook.RestBookAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
