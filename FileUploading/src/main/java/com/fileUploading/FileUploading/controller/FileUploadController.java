6b9d55f31630ec5a4d9e8568863ca7aad9944331
nligade78 <nikhilligade22@gmail.com>
2023-12-05 23:29:18 +0530

    Understanding File Uploading dynamic on target folder

--- FileUploading/src/main/java/com/fileUploading/FileUploading/controller/FileUploadController.java
+++ FileUploading/src/main/java/com/fileUploading/FileUploading/controller/FileUploadController.java
@@ -3,12 +3,14 @@ package com.fileUploading.FileUploading.controller;
 import com.fileUploading.FileUploading.services.FileUploadHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Objects;
 
 @RestController
 public class FileUploadController {
@@ -17,29 +19,33 @@ public class FileUploadController {
     private FileUploadHelper fileUploadHelper;
 
     @PostMapping("/upload-file")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file)
+    {
 //        System.out.println(file.getOriginalFilename());
 //        System.out.println(file.getSize());
 //        System.out.println(file.getContentType());
 //        System.out.println(file.getName());
         try {
-            //validation
-            if (file.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request Must Contain File");
-            }
+        //validation
+        if(file.isEmpty())
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request Must Contain File");
+        }
 
-            if (!file.getContentType().equals("image/jpeg")) {
+        if(!file.getContentType().equals("image/jpeg"))
+        {
 
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only JPEG type are allowed");
-            }
-            //file Upload Code
-            boolean f = fileUploadHelper.uploadFle(file);
-            if (f) {
-                return ResponseEntity.ok("File is Successfully uploaded");
-            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only JPEG type are allowed");
+        }
+        //file Upload Code
+           boolean f= fileUploadHelper.uploadFle(file);
+        if (f) {
+           // return ResponseEntity.ok("File is Successfully uploaded");
+            return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(Objects.requireNonNull(file.getOriginalFilename())).toUriString());
+        }
 
-
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
 
--- FileUploading/src/main/java/com/fileUploading/FileUploading/services/FileUploadHelper.java
+++ FileUploading/src/main/java/com/fileUploading/FileUploading/services/FileUploadHelper.java
@@ -1,20 +1,21 @@
 package com.fileUploading.FileUploading.services;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 @Component
 public class FileUploadHelper {
-    public final String UPLOAD_DIR = "D:\\Work\\Spring-Boot\\FileUploading\\src\\main\\resources\\static\\image";
+//    public final String UPLOAD_DIR = "D:\\Work\\Spring-Boot\\FileUploading\\src\\main\\resources\\static\\image";
+    public final String UPLOAD_DIR = new ClassPathResource("static/image").getFile().getAbsolutePath();
 
+    public FileUploadHelper() throws IOException {}
     public boolean uploadFle(MultipartFile multipartFile) {
         boolean f = false;
         try {
