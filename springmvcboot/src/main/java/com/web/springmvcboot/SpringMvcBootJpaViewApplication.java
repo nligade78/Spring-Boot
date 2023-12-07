5d77094aaea9f31990e2e5a4b10eebc6c5ce80b8
nligade78 <nikhilligade22@gmail.com>
2023-12-07 09:32:23 +0530

    Understanding lombok project and annotation (it's reducing boilerplate code for POJO class)

--- springmvcboot/src/main/java/com/web/springmvcboot/SpringMvcBootJpaViewApplication.java
+++ springmvcboot/src/main/java/com/web/springmvcboot/SpringMvcBootJpaViewApplication.java
@@ -1,5 +1,6 @@
 package com.web.springmvcboot;
 
+import com.web.springmvcboot.entity.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ public class SpringMvcBootJpaViewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcBootJpaViewApplication.class, args);
+
+		Student s=new Student();
+		s.setName("Nikhil");
+		System.out.println(s.getName());
+
 	}
 
 }
---
+++ springmvcboot/src/main/java/com/web/springmvcboot/entity/Student.java
@@ -0,0 +1,12 @@
+package com.web.springmvcboot.entity;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private int id;
+    private String name;
+    private String email;
+
+}
