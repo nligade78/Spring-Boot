c4c788a1579ef1ea41ea31c8ec05bf68c6ead5d9
nligade78 <nikhilligade22@gmail.com>
2023-12-10 23:47:02 +0530

    Understanding  adding custom CSS,JavaScript and Images in thymeleaf

--- Thymeleaf-Project/src/main/java/com/thymeleaf/Thymeleaf/Project/controller/MyController.java
+++ Thymeleaf-Project/src/main/java/com/thymeleaf/Thymeleaf/Project/controller/MyController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
