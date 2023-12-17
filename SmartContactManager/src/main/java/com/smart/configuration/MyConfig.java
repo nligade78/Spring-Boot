3dcf041e91247ffbb8b49e1d4bcabb635d8d08f6
nligade78 <nikhilligade22@gmail.com>
2023-12-17 23:51:30 +0530

    Remove Smart Contact Manager project

--- SmartContactManager/src/main/java/com/smart/configuration/MyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.smart.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MyConfig {
-
-
-}
--- SmartContactManager/src/main/java/com/smart/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smart.repository;
-
-import com.smart.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
