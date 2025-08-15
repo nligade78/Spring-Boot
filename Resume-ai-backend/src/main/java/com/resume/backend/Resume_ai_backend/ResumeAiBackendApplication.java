0e5e56bdbab0fbad75d56e789992ba79f015d474
Nikhil <nikhilligade22@gmail.com>
2025-08-15 11:43:47 +0530

    ResumeAiBackendApplication

---
+++ Resume-ai-backend/src/main/java/com/resume/backend/Resume_ai_backend/ResumeAiBackendApplication.java
@@ -0,0 +1,14 @@
+package com.resume.backend.Resume_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAiBackendApplication.class, args);
+		System.out.println("Application Started");
+	}
+
+}
