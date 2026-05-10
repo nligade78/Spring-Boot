a917993e20f597f04c7ae574108bc35e7c0898a2
Nikhil <nikhilligade22@gmail.com>
2026-05-10 22:34:14 +0530

    Modified Controller with ResponseEntity

--- empManagement/src/main/java/com/empManagement/Controller/EmployeeController.java
+++ empManagement/src/main/java/com/empManagement/Controller/EmployeeController.java
@@ -4,9 +4,12 @@ import com.empManagement.DTO.EmployeeDTO;
 import com.empManagement.Entity.Employee;
 import com.empManagement.Service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/employees")
@@ -17,36 +20,70 @@ public class EmployeeController {
 
     //POST
     @PostMapping
-    public Employee createEmployee(@RequestBody Employee employee)
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee)
     {
-       return employeeService.saveEmployee(employee);
+        Employee emp=null;
+        try
+        {
+            emp=this.employeeService.saveEmployee(employee);
+            System.out.println(emp);
+            return ResponseEntity.of(Optional.of(emp));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     //GET ALL
     @GetMapping
-    public List<EmployeeDTO> getAllEmployee()
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployee()
     {
-       return employeeService.getAllEmployee();
+        List<EmployeeDTO> empList=employeeService.getAllEmployee();
+        if(empList.size()<=0)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).body(empList);
     }
+
 
     //GET BY ID
     @GetMapping("/{id}")
-    public EmployeeDTO getEmployeeByID(@PathVariable Long id)
+    public ResponseEntity<EmployeeDTO> getEmployeeByID(@PathVariable Long id)
     {
-       return employeeService.getEmployeeByID(id);
+        EmployeeDTO employee= employeeService.getEmployeeByID(id);
+        if(employee == null)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(employee));
     }
 
     //UPDATE
     @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee)
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee)
     {
-        return employeeService.updateEmployee(id,employee);
+//        return employeeService.updateEmployee(id,employee);
+       Employee employeeUpdate= employeeService.updateEmployee(id,employee);
+       if(employeeUpdate == null)
+       {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+       }
+       return ResponseEntity.of(Optional.of(employeeUpdate));
     }
 
     //DELETE
 
-    public String deleteEmployee(@PathVariable Long id)
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id)
     {
-        return employeeService.deleteEmployee(id);
+//        return employeeService.deleteEmployee(id);
+        String deleteEmployee = employeeService.deleteEmployee(id);
+        if(deleteEmployee == null)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
